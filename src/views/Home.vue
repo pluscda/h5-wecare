ee2aa2557b31010165090b62e23e7950d66ab1a4
alex <pluscda@gmail.com>
2019-11-29 16:13:34 +0800

    modify files

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <van-button type="default">默认按钮</van-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
