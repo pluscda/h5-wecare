3d6fce4f4f536fb1ee788c29fa1341310694e0a1
alex <pluscda@gmail.com>
2019-12-03 08:22:25 +0800

    modify files

--- src/views/Carelicense.vue
+++ src/views/Carelicense.vue
@@ -3,9 +3,8 @@
     <h4>看護證照請上傳</h4>
     <div class="licensebox">
       <div class="box">
-        <img :src="img[0]" width="100%" >
+        <img :src="img[0]" width="100%" />
         <h2>看護證照(正面)</h2>
-
       </div>
       <div class="Img">
         <img :src="img[1]" />
@@ -14,65 +13,60 @@
     </div>
     <div class="licensebox">
       <div class="box">
-        <img :src="img[0]" width="100%" >
+        <img :src="img[0]" width="100%" />
         <h2>看護證照(背面)</h2>
-
       </div>
       <div class="Img">
         <img :src="img[1]" />
         <img :src="img[2]" />
       </div>
     </div>
-
-
   </div>
 </template>
 
 <script>
-export default{
-  data(){
+export default {
+  data() {
     return {
       img: [
         require("@/assets/images/carelicense/photo.png"),
         require("@/assets/images/carelicense/photoupdate.png"),
-        require("@/assets/images/carelicense/cameraupdate.png"),
+        require("@/assets/images/carelicense/cameraupdate.png")
       ]
-    }
+    };
   }
-}
+};
 </script>
 
 <style>
-*{
+* {
   /* border: 1px solid; */
 }
-.licensebox{
+.licensebox {
   width: ;
 }
-h4{
+h4 {
   text-align: left;
   padding: 15px;
   margin-top: 5px;
 }
-.box img{
+.box img {
   width: 30%;
   border-radius: 50%;
   background-color: white;
 }
-.Img{
+.Img {
   /* position: relative;
   float: right; */
   /* float: right; */
 }
-.box{
-  width:80%;
-  background-color: #F8F8F8;
+.box {
+  width: 80%;
+  background-color: #f8f8f8;
   margin: 20px 0px;
   margin-left: auto;
   margin-right: auto;
   padding: 30px 0px;
   border-radius: 5%;
-
 }
-
 </style>
